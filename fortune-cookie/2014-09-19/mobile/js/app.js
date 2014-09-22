27130864d24e771bffbb1e2d93f2296278fab4b1
Carolina <racoquita@me.com>
2014-09-21 22:02:23 -0400

    Mobile Slices

--- fortune-cookie/2014-09-19/mobile/js/app.js
+++ fortune-cookie/2014-09-19/mobile/js/app.js
@@ -1,10 +1,58 @@
 var App = function() {
-	var that = this;
+	var that = this
+	,	fortunes = [
+		'don’t look before you leap.',
+		'expect the worst. prepare for the best.',
+		'it doesn’t pay to plan ahead.'
+	]
 
 	this.on = function() {
+		$('.cta').on('click', function(e){
+			e.preventDefault();
 
+			$('.cta').removeClass('show');
+			$('.cookie').addClass('animate');
+
+			setTimeout(function(){
+				$('.resizable').resizable({disabled: false});
+				$('.prediction').text(fortunes[Math.floor(Math.random() * 3)]);
+				$('.fortune').addClass('show');
+				$('.resizable').resizable({
+					minWidth: 60,
+					maxWidth: 305,
+					handles: 'e',
+					stop: function() {
+						that.showModal();
+					}
+				});
+			}, 400);
+		});
+
+		$('.another').on('click', function(e){
+			e.preventDefault();
+			that.resetFortune();
+
+			$('.modal').velocity('fadeOut', {
+				duration: 500
+			});
+		});
 	}
 	this.off = function() {
-
+		$('.cta').off();
+		$('.modal').hide();
+		that.resetFortune();
+	}
+	this.showModal = function() {
+		$('.modal').velocity('fadeIn', {
+			delay: 2000,
+			duration: 500
+		});
+	}
+	this.resetFortune = function() {
+		$('.resizable').resizable({disabled: true});
+		$('.cta').addClass('show');
+		$('.fortune').removeClass('show').removeAttr('style');
+		$('.cookie').removeClass('animate');
+		$('.resizable').removeAttr('style');
 	}
 };
